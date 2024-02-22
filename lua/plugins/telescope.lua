5bdfca5827602920e44a4f15266f3d43f11a09a9
Dat <trandatnh@gmail.com>
2024-02-21 22:55:18 -0800

    Update telescope config
    Adding keymap to brow plugin files
    move prompt positions to top
    display path smart

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,20 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    -- add a keymap to browse plugin files
+    -- stylua: ignore
+    {
+      "<leader>fp",
+      function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+      desc = "Find Plugin File",
+    },
+  },
+  opts = {
+    defaults = {
+      layout_config = { prompt_position = "top" },
+      -- remove as much from the path as possible to only show
+      -- the difference between the displayed paths.
+      path_display = { "smart" },
+    },
+  },
+}
