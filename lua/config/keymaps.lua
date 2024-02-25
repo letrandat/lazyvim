c0bd1ea4b8bbfd319f7817b596a69e09ded2ea9e
Dat <trandatnh@gmail.com>
2024-02-25 11:04:01 -0800

    config floating terminal ctrl-`
    to be similar with other terminal I use

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- floating terminal
+local Util = require("lazyvim.util")
+
+-- Terminal Mappings
+local lazyterm = function()
+  Util.terminal(nil, { cwd = Util.root() })
+end
+vim.keymap.set("n", "<c-`>", lazyterm, { desc = "Terminal (root dir)" })
+vim.keymap.set("t", "<C-`>", "<cmd>close<cr>", { desc = "Hide Terminal" })
